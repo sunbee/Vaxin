924c59eab7bbc12bf45e360220c8132f17b0b0b0
sunbee <sanjay.bhatikar@gmail.com>
2023-08-18 18:06:20 +0530

    Added feature to show image in circle shape.

--- app/src/main/java/com/example/vaxin/ui/show_detail/ShowDetailScreen.kt
+++ app/src/main/java/com/example/vaxin/ui/show_detail/ShowDetailScreen.kt
@@ -1,12 +1,21 @@
 package com.example.vaxin.ui.show_detail
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.rememberAsyncImagePainter
@@ -17,12 +26,22 @@ fun ShowDetailScreen(
     vaccineId: String,
     viewModel: ShowDetailViewModel = hiltViewModel()
 ) {
-    Column {
+    Column(
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth()
+    ) {
         Text(text = vaccineId)
+        Spacer(modifier = Modifier.height(8.dp))
+
         Image(
             painter = rememberAsyncImagePainter(model = SampleData.vaccineImages.get(vaccineId)),
             contentDescription = "CDC Image",
-            modifier = Modifier.size(256.dp) )
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(16.dp)
+                .size(256.dp)
+                .clip(CircleShape))
         Text(text = viewModel.vaccineId)
     }
 }
